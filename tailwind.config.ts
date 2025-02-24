6e9a981e239e8f7e056118c2dec950483131d1b1
ak-pydev <ak.iscoding@gmail.com>
2025-02-24 07:20:10 -0500

    checking components

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,15 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      keyframes: {
+        gradient: {
+          '0%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+          '100%': { backgroundPosition: '0% 50%' },
+        },
+      },
+      animation: {
+        gradient: 'gradient 8s linear infinite'
       },
     },
   },
